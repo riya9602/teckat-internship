16a1ba138f8364132f7a6bde73fa36bf2d4b0724
riya <riyagond9602@gmail.com>
2024-06-20 18:23:55 +0530

    create- student

--- src/pages/Student.tsx
+++ src/pages/Student.tsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React , {useEffect, useState} from "react";
+import {IStudent} from "../interface/student";
+import { Link } from "react-router-dom";
 
-const Student = () => {
+const Student: React.FC = () => {
+  const [allStudents,setAllStudent]=useState<IStudent[]>();
+  useEffect(() =>{
+    const Student = JSON.parse(localStorage.getItem("students") as string);
+    setAllStudent(Student)
+    console.log("student data",Student);
+  },[])
+;  
   return (
-   <><button> <Link to="/student/create-student">Create-Students</Link></button></>
-  )
-}
+   <><button> <Link to="/student/create-student">Create-Students</Link></button>
+   <table>
+        <thead>
+          <tr>
+            <td>SN.</td>
+            <td>Name</td>
+            <td>Age</td>
+            <td>Phone Number</td>
+            <td>Email ID</td>
+          </tr>
+        </thead>
+        <tbody>
+          {allStudents?.map((item, index) => {
+            return (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{item.name}</td>
+                <td>{item.age}</td>
+                <td>{item.phone}</td>
+                <td>{item.email}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      </>
+  );
+};
 
-export default Student
+export default Student;
--- src/pages/createstudent.tsx
+++ src/pages/createstudent.tsx
@@ -12,16 +12,18 @@ const CreateStudent: React.FC = () => {
   } = useForm<IStudent>({ mode: "all" });
 
   const createNewStudent = (data: IStudent) => {
-    console.log(data);
-localStorage.setItem("students", JSON.stringify(data));
-    // const students: IStudent[] = JSON.parse(
-    //   localStorage.getItem("students") as string
-    // );
-    // data._id = (students.length + 1).toString();
+    let students: IStudent[] = [];
 
-    // students.push(data);
-    // localStorage.setItem("students", JSON.stringify(students));
-    // navigate("/student/student-details");
+    const newStudent = JSON.parse(localStorage.getItem("students") as string);
+    if (newStudent) {
+      data._id = (students.length + 1).toString();
+      students = newStudent;
+    } else {
+      data._id = (0).toString();
+    }
+    students.push(data);
+    localStorage.setItem("students", JSON.stringify(students));
+    navigate("/student");
   };
   return (
     <>
